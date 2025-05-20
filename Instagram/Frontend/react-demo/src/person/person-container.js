05b993d0c6f43dcfdf31ddad15eeea1f9504cc9f
RazviOne <razvan.coroian@gmail.com>
2025-05-20 18:07:12 +0300

    Fixed the issue with person page

--- Instagram/Frontend/react-demo/src/person/person-container.js
+++ Instagram/Frontend/react-demo/src/person/person-container.js
@@ -29,28 +29,15 @@ class PersonContainer extends React.Component {
             error: null,
             notifications: []
         };
-
-        this.stompClient = null;
-        this.stompChatClient = null;
     }
 
     componentDidMount() {
         // console.log('componentDidMount()');
         // this.protectRoute();
-        this.fetchPersons();
-        this.connectWebSocket();
-        this.connectChatWebSocket();
+        // this.fetchPersons();
     }
 
     componentWillUnmount() {
-        if (this.stompClient) {
-            this.stompClient.deactivate();
-            // console.log('WebSocket connection closed.');
-        }
-        if (this.stompChatClient) {
-            this.stompChatClient.deactivate();
-            // console.log('WebSocket connection closed.');
-        }
     }
 
     protectRoute() {
@@ -122,131 +109,17 @@ class PersonContainer extends React.Component {
         });
     }
 
-    connectWebSocket() {
-        // console.log('connectWebSocket()');
-
-        const { user } = this.context;
-
-        const socket = new SockJS(HOST.measurements_api + '/ws-message');
-
-        this.stompClient = new Client({
-            webSocketFactory: () => socket,
-            reconnectDelay: 5000,
-            onConnect: (frame) => {
-                // console.log('Connected to WebSocket:', frame);
-
-                this.stompClient.subscribe('/topic/message', (message) => {
-                    const websocketMessage = JSON.parse(message.body);
-                    // console.log('Received update:', websocketMessage);
-                    // console.log(`Notification: ${websocketMessage.notification}`);
-
-                    const personId = websocketMessage.personId;
-                    // console.log(`Person ID: ${personId}, ${typeof(personId)}`);
-
-                    if(personId !== undefined) {
-                        API_USERS.getPersonById(personId, (result, status, error) => {
-                            if (result !== null && (status === 200 || status === 201)) {
-                                // console.log(`Person found: ${result.username}`);
-                                if (user.username === result.username) {
-                                    const notificationId = Date.now() + Math.random();
-
-                                    this.setState((prevState) => ({
-                                        notifications: [...prevState.notifications, {
-                                            id: notificationId,
-                                            message: websocketMessage.notification
-                                        }]
-                                    }));
-
-                                    this.reload();
-                                }
-                            } else {
-                                this.setState({
-                                    isLoaded: false,
-                                    errorStatus: status,
-                                    error: error
-                                });
-                            }
-                        });
-                    }
-                });
-            },
-            onStompError: (frame) => {
-                console.error('WebSocket error:', frame);
-            }
-        });
-
-        this.stompClient.activate();
-    }
-
-    connectChatWebSocket() {
-        console.log('connectChatWebSocket()');
-
-        const { user } = this.context;
-
-        const socket = new SockJS(HOST.chat_api + '/ws-chat');
-
-        this.stompChatClient = new Client({
-            webSocketFactory: () => socket,
-            reconnectDelay: 5000,
-            onConnect: (frame) => {
-                console.log('Connected to Chat WebSocket:', frame);
-
-                this.stompChatClient.subscribe('/topic/message', (message) => {
-                    const websocketMessage = JSON.parse(message.body);
-                    console.log('Received update:', websocketMessage);
-
-
-                    const messageType = websocketMessage.type;
-                    // console.log(`Message type: ${messageType}, ${typeof(messageType)}`);
-
-                    // const userId = websocketMessage.userId;
-                    // console.log(`New message for user ${userId}`);
-
-                    console.log(`Current user id: ${user.id}`);
-                    if(messageType === 'MessageReceive') {
-                        // Display the message
-                        // Pop up the chat box, if it's not shown
-                        // Push the message in the chat
-                        console.log("New message from admin");
-                    }
-                });
-            },
-            onStompError: (frame) => {
-                console.error('Chat WebSocket error:', frame);
-            }
-        });
-
-        this.stompChatClient.activate();
-    }
-
     reload() {
         // console.log('reload()');
         this.setState({ isLoaded: false });
-        this.fetchPersons();
+        // this.fetchPersons();
     }
-
-    dismissNotification = (id) => {
-        this.setState((prevState) => ({
-            notifications: prevState.notifications.filter(notification => notification.id !== id)
-        }));
-    };
 
     render() {
         // console.log('render()');
         const { user } = this.context;
         return (
             <div>
-                <div className="notification-container">
-                    {this.state.notifications.map((notification, index) => (
-                        <Notification
-                            key={notification.id}
-                            id={notification.id}
-                            message={notification.message}
-                            index={index}
-                            onClose={this.dismissNotification}
-                        />
-                    ))}
-                </div>
                 <CardHeader>
                     <strong> Welcome {user ? user.username : ''} </strong>
                 </CardHeader>
@@ -263,24 +136,13 @@ class PersonContainer extends React.Component {
                     </Row>
                     <br />
                     <Row>
-                        <Col><strong>Devices</strong></Col>
-                    </Row>
-                    <Row>
                         <Col><strong>Postări</strong></Col>
-                        </Row>
-                        <Row>
-                        <Col sm={{ size: '8', offset: 1 }}>
-                            <Feed />
-                        </Col>
                     </Row>
-                    <Row>
-                        <Col sm={{ size: '8', offset: 1 }}>
-                            {this.state.isLoaded && <DeviceTable tableData={this.state.tableData} />}
-                            {this.state.errorStatus > 0 && (
-                                <APIResponseErrorMessage errorStatus={this.state.errorStatus} error={this.state.error} />
-                            )}
-                        </Col>
-                    </Row>
+                    {/*<Row>*/}
+                    {/*    <Col sm={{ size: '8', offset: 1 }}>*/}
+                    {/*        <Feed />*/}
+                    {/*    </Col>*/}
+                    {/*</Row>*/}
                 </Card>
             </div>
         );
